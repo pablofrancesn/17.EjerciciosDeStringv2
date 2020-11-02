a6877835c21c8a9573d45c0db56feeb910695145
Pablo Francés <pablofrances.n@gmail.com>
2020-11-02 15:33:03 +0100

    Ej9

--- src/Ejercicio7.java
+++ src/Ejercicio7.java
@@ -5,18 +5,24 @@ public class Ejercicio7 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int counter = 0;
+		int aCounter = 0, oCounter = 0, iCounter = 0;
 		for (int i = 0; i < 50; i++) {
 			System.out.print("Introduce un nombre: ");
 			String name = sc.nextLine();
 			for (int j = 0; j < name.length(); j++) {
-				if (name.endsWith() || name.charAt(j) == 'E') {
-					counter++;
+				if (name.endsWith("a") || name.endsWith("A")) {
+					aCounter++;
+				}if (name.endsWith("o") || name.endsWith("O")) {
+					oCounter++;
+				}if (name.endsWith("i") || name.endsWith("I")) {
+					iCounter++;
 				}
 			}
 			System.out.print("");
 		}
-		System.out.println("Hay: " + counter + " es.");
+		System.out.println("Hay: " + aCounter + " nombres acabados en A.");
+		System.out.println("Hay: " + oCounter + " nombres acabados en O.");
+		System.out.println("Hay: " + iCounter + " nombres acabados en I.");
 	}
 
 }
